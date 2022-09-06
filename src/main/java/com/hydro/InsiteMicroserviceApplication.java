6e11ed0c55aa355821b73d6b092dfbacd0096110
Samuel Butler <sambutler1017@icloud.com>
2022-09-06 13:23:23 -0400

    no message

--- src/main/java/com/hydro/InsiteMicroserviceApplication.java
+++ src/main/java/com/hydro/InsiteMicroserviceApplication.java
@@ -3,9 +3,6 @@ package com.hydro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
@@ -14,10 +11,5 @@ public class InsiteMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsiteMicroserviceApplication.class, args);
-	}
-
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
 	}
 }
